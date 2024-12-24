36f01f7488e348e8c6ac0b72bb2902c1eaaf055f
helllopratik <pratikgondane07@gmail.com>
2024-12-25 03:28:49 +0530

    add aosp dependencies for linux

--- android build/build.sh
+++ android build/build.sh
@@ -2,8 +2,11 @@
 export set ALLOW_MISSING_DEPENDENCIES=TRUE
 set ALLOW_MISSING_DEPENDENCIES=TRUE
 # Choose a target
-lunch evolution_kunlun2_row-userdebug
+#lunch evolution_kunlun2_row-userdebug
+lunch lineage_kunlun2_row-ap3a-userdebug
 
+lunch lineage_kunlun2_row-ap3a-userdebug
+m evolution
 # Build the code
 mka bacon -j$(nproc --all)
 sleep 234
--- pre.sh
+++ pre.sh
@@ -2,3 +2,6 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install gh
 gh auth login
+
+sudo apt install libwxgtk-webview3.2-1 libwxgtk-webview3.2-dev -y
+sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev default-jdk libwxgtk-webview3.2-1 libwxgtk-webview3.2-dev automake
