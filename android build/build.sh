c2bf0b6733fa94b8488b8fe5fcdd650a8e6e22a9
helllopratik <pratikgondane07@gmail.com>
2024-12-24 15:32:48 +0530

    change lineage to evolution

--- android build/build.sh
+++ android build/build.sh
@@ -2,7 +2,7 @@
 export set ALLOW_MISSING_DEPENDENCIES=TRUE
 set ALLOW_MISSING_DEPENDENCIES=TRUE
 # Choose a target
-lunch lineage_kunlun2_row-userdebug
+lunch evolution_kunlun2_row-userdebug
 
 # Build the code
 mka bacon -j$(nproc --all)
